0bd3e1c3d1a3a0736769b6fcf8ed9aa098b4d086
farruq <nazfar1988@gmail.com>
2026-09-04 18:19:36 +0500

    populate script

--- update_search.sh
+++ update_search.sh
@@ -3,8 +3,13 @@
 
 echo "Starting Pelias Search Engine update..."
 
+
+echo "Creating Pelias Elasticsearch Schema..."
+docker run --rm   --network franki_map_default   -v /Users/farruqnazri/Desktop/franki_map/pelias.json:/code/pelias.json   pelias/schema npm run create_index
+
+echo "Importing CSV data..."
 docker run --rm \
-  --network frankenmap_default \
+  --network franki_map_default \
   -v $(pwd)/pelias.json:/code/pelias.json \
   -v $(pwd)/gh-data:/data \
   pelias/csv-importer npm start
